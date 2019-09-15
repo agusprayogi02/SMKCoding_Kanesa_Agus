faba2276bb547c4e11854c8485cfd5a63729822c
Agus Prayogi <agusyogi600@gmail.com>
2019-09-15 11:03:34 +0700

    commit10

--- Canteen/app/src/main/java/id/canteen/ui/login/LoginActivity.kt
+++ Canteen/app/src/main/java/id/canteen/ui/login/LoginActivity.kt
@@ -16,7 +16,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_login)7
         add.setOnClickListener {
             Masuk()
         }
